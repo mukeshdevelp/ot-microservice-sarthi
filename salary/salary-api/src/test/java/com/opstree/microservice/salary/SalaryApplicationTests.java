f1e334fbed9f93fd520bdb06f56960e516f4a892
mukeshdevelp <163710035+mukeshdevelp@users.noreply.github.com>
2026-03-30 12:54:36 +0530

    uncommit all the changed in salaryTest.java file for ut

--- salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -2,15 +2,8 @@ package com.opstree.microservice.salary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//added lines for unit testing
-import org.springframework.test.context.ActiveProfiles;
-@SpringBootTest(
-  properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration,org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration"
-  }
-)
-// added lines for unit testing
-@ActiveProfiles("test")
+
+@SpringBootTest
 class SalaryApplicationTests {
 
 	@Test
