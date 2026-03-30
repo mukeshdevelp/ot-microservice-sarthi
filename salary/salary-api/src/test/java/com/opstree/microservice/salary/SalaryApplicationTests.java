a148102f0c9141f0724be179c85b2779a698a04c
mukeshdevelp <163710035+mukeshdevelp@users.noreply.github.com>
2026-03-30 12:45:21 +0530

    Enable test profile for SalaryApplicationTests
    Added ActiveProfiles annotation for unit testing.

--- salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -2,8 +2,11 @@ package com.opstree.microservice.salary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//added lines for unit testing
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+// added lines for unit testing
+@ActiveProfiles("test")
 class SalaryApplicationTests {
 
 	@Test
