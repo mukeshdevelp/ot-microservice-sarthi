1d39b9d79006a8f96e010783963b7b7f702c997a
mukeshdevelp <163710035+mukeshdevelp@users.noreply.github.com>
2026-03-30 12:49:35 +0530

    adding changed for skip test

--- salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 //added lines for unit testing
 import org.springframework.test.context.ActiveProfiles;
-@SpringBootTest
+@SpringBootTest(
+  properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration,org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration"
+  }
+)
 // added lines for unit testing
 @ActiveProfiles("test")
 class SalaryApplicationTests {
