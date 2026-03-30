d4d396a44d7c09b571e865096d90a982b06fc926
mukeshdevelp <163710035+mukeshdevelp@users.noreply.github.com>
2026-03-30 13:02:29 +0530

    mocking the bean for scylladb

--- salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ salary/salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SalaryApplicationTests {
+	@MockBean
+    private com.datastax.oss.driver.api.core.CqlSession cqlSession;
 
 	@Test
 	void contextLoads() {
